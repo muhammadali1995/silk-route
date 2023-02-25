f10bbe7e9abe8eff96d5d7eb8d01852d4e561eca
Olamgir <ollomurodovolamgir233@gmail.com>
2023-02-25 16:52:30 +0500

    about page changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import arrowUp from "../../assets/icons/ArrowUp.svg";
+import goal from "../../assets/imgs/Icon_goal-24.png";
+import gppGood from "../../assets/imgs/gpp-good.png";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/imgs/logo.png";
 
 const About = () => {
   const [showInfo, setShowInfo] = useState(false);
@@ -18,12 +22,12 @@ const About = () => {
           </span>
         </p>
       </header>
-
+      {/* first section */}
       <section className=" bg-electricPurple py-24 text-center ">
         <p className=" text-vividPink font-anton text-5xl">
           Hi, we’re Silk Route.
         </p>
-        <div className=" font-ptSansNarrow text-white text-xl text-center mx-80 pt-8">
+        <div className=" font-ptSansNarrow text-white text-xl text-center mx-auto pt-8 max-w-3xl leading-9">
           <p>
             {" "}
             We’re an immigration consultancy firm that specializes in helping
@@ -66,14 +70,14 @@ const About = () => {
             {showInfo ? (
               <img
                 onClick={() => setShowInfo(false)}
-                className=" h-8"
+                className=" h-8 cursor-pointer"
                 src={arrowUp}
                 alt=""
               />
             ) : (
               <button
                 onClick={() => setShowInfo(true)}
-                className=" text-delayedYellow font-bold font-ptSansNarrow"
+                className=" text-delayedYellow font-bold font-ptSansNarrow cursor-pointer  underline decoration-delayedYellow    "
               >
                 Read more
               </button>
@@ -81,15 +85,17 @@ const About = () => {
           </div>
         </div>
       </section>
-
-      <aside className=" bg-vividPink py-24 text-center ">
-        <p className=" text-springGreen font-anton text-5xl">Mission statement</p>
-        <p className="font-anton text-electricPurple">
+      {/* second section */}
+      <section className=" bg-vividPink py-24 text-center  ">
+        <p className=" text-springGreen font-anton text-5xl">
+          Mission statement
+        </p>
+        <p className="font-anton text-title28 max-w-6xl leading-10 mx-auto text-electricPurple py-6">
           Empowering people to begin new lives in the European Union through
           business ownership.
         </p>
-        <div>
-          <p className=" font-ptSansNarrow text-black text-xl text-center mx-80 pt-8">
+        <div className="flex gap-4 mx-auto max-w-6xl ">
+          <p className=" font-ptSansNarrow text-black text-xl  pt-8 text-justify leading-9">
             Our mission is to simplify the process of immigration by meeting you
             where you are and working with what you have, so that your dream of
             a new life becomes a reality. Treating you like family from start to
@@ -97,9 +103,71 @@ const About = () => {
             situation–companies owned and run by you — that establish EU
             residency and allow you to discover the possibilities of a new life.
           </p>
-          <img src="" alt="" />
+
+          <img className=" w-52" src={goal} alt="" />
         </div>
-      </aside>
+      </section>
+      {/* third section */}
+      <section className=" bg-springGreen py-24 text-center  ">
+        <p className=" text-vividPink  font-anton text-5xl">Vision statement</p>
+        <p className="font-anton text-title28 text-electricPurple max-w-6xl leading-10 mx-auto py-6">
+          Striving to be the agency of choice for individuals seeking new life
+          life in the European union.
+        </p>
+        <div className="flex gap-2 mx-auto max-w-6xl ">
+          <img className=" w-52" src={gppGood} alt="" />
+          <p className=" font-ptSansNarrow text-black text-xl  pt-8 text-justify leading-9">
+            It doesn’t take a crystal ball to see where our company is headed.
+            Our dream is to become a global leader in immigration affairs
+            concerning individuals who want to plant roots in Europe. We are in
+            the business of helping people begin new lives in the EU, and we
+            approach this both practically and sustainably. Creating and
+            planting businesses is a practical approach to establishing EU
+            residency, while giving full autonomy of the company to the
+            individual allows them to sustain their success after arrival. Silk
+            Route’s intention is to be one of the best residency programs in
+            Europe. We trust that our future promises stories of new beginnings
+            and lifelong successes.
+          </p>
+        </div>
+      </section>
+      {/* footer */}
+      <footer className="bg-black">
+        <div className=" flex justify-between px-24 pt-1 font-ptSansNarrow">
+          <div>
+            <img src={logo} alt="" />
+          </div>
+          <ul className="flex gap-14 list-none text-white text-nabar pr-16 pt-8">
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Home</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>About Us</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Services</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Blogs</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>FAQ</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Contact </NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Terms and Conditions</NavLink>
+            </li>
+            <li className=" hover:text-delayedYellow">
+              <NavLink>Privacy Policy</NavLink>
+            </li>
+          </ul>
+        </div>
+        <p className="text-center font-ptSansNarrow text-white pt-11">
+          Silk Route. All rights reserved © 2023
+        </p>
+      </footer>
     </div>
   );
 };
