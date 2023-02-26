1222ceed201bf9d362e31ff37b67ef3e29b2c578
Olamgir <ollomurodovolamgir233@gmail.com>
2023-02-26 17:53:53 +0500

    Faq  page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 // import Home from "./pages/Home";
 import About from './components/About/About'
+import Faq from "./components/Faq/Faq";
 
 function App() {
   return (
     <Routes>
       {/* <Route path="/" element={<Home />} /> */}
       <Route path="/" element={<About />} />
+      <Route path="/faq" element={<Faq />} />
     </Routes>
 
   );
