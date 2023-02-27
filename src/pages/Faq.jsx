92ef5d1432fa71c28924739e9e39b00c7e913baf
Olamgir <ollomurodovolamgir233@gmail.com>
2023-02-27 16:21:36 +0500

    Faq page finished

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,72 +1,82 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
 import { data } from "../constants/FaqData";
-import answerIcon from "./../assets/imgs/answerIcon.png";
-import yellowArrow from "./../assets/imgs/yellowArrow.png";
-import greenArrow from "./../assets/imgs/greenArrow.png";
-import questionIcon from "./../assets/imgs/questionIcon.png";
 
 const Faq = () => {
   return (
-    <div className="bg-vividPink font-ptSansNarrow  pb-16  ">
-      <Navbar />
-      <div className="pt-20">{data[0].map((item, index) => {
-        return (
-          <div key={index}>
-            <div className="flex gap-1 max-w-[948px] mx-auto pt-8 pb-4">
-              <div className="flex gap-1">
-                <img src={questionIcon} alt="question" />
-                <h3 className=" text-springGreen font-anton text-3xl  ">
-                  Question
-                </h3>
-                <img src={greenArrow} alt="arrow" />
-              </div>
+    <div className="bg-vividPink font-ptSansNarrow  py-16  ">
+      <div className="pt-20">
+        {data[0].map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="max-w-[947px] mx-auto w-full flex flex-col justify-center lg:px-0 sm:px-4 px-2"
+            >
+              <div className="flex gap-1 lg:text-3xl text-2xl  pt-8 pb-4">
+                <div className="flex gap-1 ">
+                <span className="text-springGreen lg:text-4xl text-3xl ">&#10070;</span>
+                  <h3 className=" text-springGreen font-anton  ">
+                    Question
+                  </h3>
+                  <span className="text-springGreen">&#10132;</span>
+                </div>
 
-              <p className="text-springGreen text-3xl">{item.question}</p>
-            </div>
-            <div className="flex gap-1 max-w-[890px] mx-auto">
-              <div className="flex gap-1 mx-w-[120px] h-min pl-1">
-                <img className="max-w-[25px] w-full" src={answerIcon} alt="" />
-                <h4 className="text-delayedYellow text-2xl font-anton ">
-                  Answer
-                </h4>
-                <img src={yellowArrow} alt="" />
+                <p className="text-springGreen lg:text-3xl text-2xl">
+                  {item.question}
+                </p>
               </div>
-              <p className="text-delayedYellow text-justify max-w-[770px] text-xl">
-                {item.answer}
-              </p>
-            </div>
-          </div>
-        );
-      })}
-      {data[1].map((item, index) => {
-        return (
-          <div key={index}>
-            <div className="flex gap-1 max-w-[977px] text-left mx-auto pt-8 pb-4">
-              <div className="flex gap-1">
-                <img src={questionIcon} alt="question" />
-                <h3 className=" text-springGreen font-anton text-3xl  ">
-                  Question
-                </h3>
-                <img src={greenArrow} alt="arrow" />
+              <div className="flex lg:pl-[60px] pl-8  justify-center text-center">
+                <div className="flex  pt-[10px] gap-1 items-center justify-center mx-w-[120px] h-min">
+                  <span className=" scale-125 text-delayedYellow  ">
+                    &#9670;
+                  </span>
+                  <h4 className="text-delayedYellow text-2xl font-anton ">
+                    Answer
+                  </h4>
+                  <span className="text-delayedYellow">&#10132;</span>
+                </div>
+                <p className="text-delayedYellow pt-[15px] text-justify max-w-[770px] lg:text-xl text-[18px]">
+                  {item.answer}
+                </p>
               </div>
-              <p className="text-springGreen text-3xl">{item.question}</p>
             </div>
-            <div className="flex gap-1 max-w-[990px] mx-auto">
-              <div className="flex gap-1 mx-w-[120px] h-min pl-10">
-                <img src={answerIcon} alt="" />
-                <h4 className="text-delayedYellow text-2xl font-anton ">
-                  Answer
-                </h4>
-                <img src={yellowArrow} alt="" />
+          );
+        })}
+        {data[1].map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="max-w-[977px] mx-auto w-full flex flex-col justify-center lg:px-0 sm:px-4 px-2"
+            >
+              <div className="flex gap-1  text-left  pt-8 pb-4">
+                <div className="flex lg:text-3xl text-2xl  gap-1">
+                <span className="text-springGreen lg:text-4xl text-3xl ">&#10070;</span>
+                  <h3 className=" text-springGreen font-anton  ">
+                    Question
+                  </h3>
+                  <span className="text-springGreen">&#10132;</span>
+                </div>
+                <p className="text-springGreen lg:text-3xl text-2xl">
+                  {item.question}
+                </p>
               </div>
-              <p className="text-delayedYellow text-justify max-w-[799px] text-xl">
-                {item.answer}
-              </p>
+              <div className="flex lg:pl-[60px] pl-8 gap-1 ">
+                <div className="flex pt-[10px] gap-1 mx-w-[120px] h-min ">
+                  <span className=" scale-125 text-delayedYellow  ">
+                    &#9670;
+                  </span>
+                  <h4 className="text-delayedYellow text-2xl font-anton ">
+                    Answer
+                  </h4>
+                  <span className="text-delayedYellow">&#10132;</span>
+                </div>
+                <p className="text-delayedYellow pt-[15px] text-justify  max-w-[799px] lg:text-xl text-[18px]">
+                  {item.answer}
+                </p>
+              </div>
             </div>
-          </div>
-        );
-      })}</div>
+          );
+        })}
+      </div>
     </div>
   );
 };
