7028938c1e503c8b387267e28d6dea139f1fecf0
muhammadali1995 <4377433mtDEV>
2023-03-01 06:57:48 +0500

    logo updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Container from "../Container";
 import closeMenu from './../../assets/imgs/closeMenu.png';
 import menu from './../../assets/imgs/menu.png';
 import NavbarLink from "./NavBarLink";
-import { Logo } from "../Icons";
+import { Logo, LogoMobile } from "../Icons";
 
 const Navbar = () => {
 
@@ -21,8 +21,9 @@ const Navbar = () => {
     <div className="absolute w-full main top-0">
       <Container>
         <div className="w-full lg:my-4 flex justify-between items-center md:px-20   lg:py-2.5 py-5 px-2.5 lg:pt-0  font-ptSansNarrow">
-          <div className="z-10 lg:mt-2 md:ml-0 ml-4 md:w-[141px] w-[122px]">
-            <Logo className="w-full"/>
+          <div className="z-10 lg:mt-2 md:ml-0 ml-4">
+            <LogoMobile className="md:hidden" />
+            <Logo className="hidden md:block" />
           </div>
           <NavLinks className={'lg:flex gap-14 list-none text-white text-nabar pr-12 -mt-3 hidden'} />
           <div className="lg:hidden md:w-12 w-7 mr-7 cursor-pointer z-10" onClick={() => setMenuIsOpen(!menuIsOpen)}>
