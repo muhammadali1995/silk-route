3050f7cecba0c1e0b2a5385177d62349e0607daa
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-03-02 16:14:34 +0500

    contact updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
             <Container>
                 <h1 className='font-anton mx-2 lg:mt-28 mt-10 lg:text-4xl text-3xl text-electricPurple text-center'>Let’s explore how Silk Route can help you achieve your dreams of a new life!!!</h1>
                 <h1 className='mt-8 font-anton mx-2 lg:text-4xl text-3xl text-vividPink text-center'>Click here to contact a member of our team</h1>
-                <div className='flex flex-col lg:h-screen justify-between'>
+                <div className='flex flex-col lg:min-h-screen justify-between'>
                     <div>
                         <div className='w-full flex justify-center mt-7'>
                             <img
